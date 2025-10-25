d214e0a017a6a5eaf918aaa1df8bf493fba599cf
John Forster <john.forster@makers.tech>
2025-10-25 20:49:03 +0100

    Update logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,17 @@
 import type { ProcessingOptions } from "../types";
 
+const FIREBASE_URL = "https://logusage-dvbaqkhwga-uc.a.run.app";
+
 export const logUsage = async (file: File, extraInfo: ProcessingOptions) => {
   // Only log usage in production environment
   if (!import.meta.env.PROD) {
     return;
   }
 
-  let FIREBASE_URL = "https://logusage-dvbaqkhwga-uc.a.run.app ";
   fetch(FIREBASE_URL, {
     method: "POST",
     body: JSON.stringify({
+      app: "icon-generator",
       filename: file.name,
       filesize: file.size,
       filetype: file.type,
@@ -17,6 +19,7 @@ export const logUsage = async (file: File, extraInfo: ProcessingOptions) => {
     }),
     headers: {
       "x-password": "dungeon-mister",
+      "Content-Type": "application/json",
     },
   }).catch(console.error);
 };
